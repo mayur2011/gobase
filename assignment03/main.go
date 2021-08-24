9a5e4a1962ffca77753653acab6a0bfaf6319143
mayur2011 <er.mayur2011@gmail.com>
2021-08-24 12:06:53 +0530

    adding some of work on assignment 03

--- assignment03/main.go
+++ assignment03/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gobase/assignment03/domain"
+	"gobase/assignment03/mapstore"
+)
 
-func main(){
-fmt.Println("testing")
+type CustomerController struct {
+	store domain.CustomerStore
+}
+
+func (cc CustomerController) Add(c domain.Customer) {
+	err := cc.store.Create(c)
+	if err != nil {
+		fmt.Println("Failed to create", err)
+	}
+	fmt.Println("Customer has been added")
+}
+
+func (cc CustomerController) GetAllCustomers() {
+	customers, err := cc.store.GetAllCustomers()
+	if err != nil {
+		fmt.Println("Failed to fetch all customers", err)
+	}
+	fmt.Println(customers)
+}
+
+func main() {
+	cc := CustomerController{
+		store: mapstore.NewMapStore(),
+	}
+	customer := domain.Customer{
+		ID:    "C101",
+		Name:  "Raju",
+		Email: "raju-tech@gnn.in",
+	}
+	cc.Add(customer)
+	customer = domain.Customer{
+		ID:    "C202",
+		Name:  "Maju",
+		Email: "maju-tech@gnn.in",
+	}
+	cc.Add(customer)
+	cc.GetAllCustomers()
 }
---
+++ assignment03/mapstore/customermapstore.go
@@ -0,0 +1,38 @@
+package mapstore
+
+import (
+	"fmt"
+	"gobase/assignment03/domain"
+)
+
+type MapStore struct {
+	store map[string]domain.Customer
+}
+
+//Factory method gives a new instance of MapStore
+//Also this is for caller packages, not for mapstoer itself
+func NewMapStore() *MapStore {
+	return &MapStore{store: make(map[string]domain.Customer)}
+}
+
+func (ms *MapStore) isCustomerExists(id string) bool {
+	_, ok := ms.store[id]
+	return ok
+}
+
+func (ms *MapStore) Create(customer domain.Customer) error {
+	if ms.isCustomerExists(customer.ID) {
+		return fmt.Errorf("customer exists")
+	}
+	ms.store[customer.ID] = customer
+	fmt.Println("Customer has been created")
+	return nil
+}
+
+func (ms *MapStore) GetAllCustomers() ([]domain.Customer, error) {
+	var customers []domain.Customer
+	for k := range ms.store {
+		customers = append(customers, ms.store[k])
+	}
+	return customers, nil
+}
